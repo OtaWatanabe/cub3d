603d6849087e7441f68a1a83cc7b3cd548b1656a
Kotaro Namba <knamba@c3r4s8.42tokyo.jp>
2025-02-09 23:49:35 +0900

    update

--- includes/config.h
+++ includes/config.h
@@ -2,31 +2,39 @@
 # define CONFIG_H
 
 // 画面サイズ
-#define SCREEN_WIDTH  1024
-#define SCREEN_HEIGHT 768
+# define SCREEN_WIDTH 1024
+# define SCREEN_HEIGHT 768
 
 // Linux環境のキーコード
 // (X11/keysym.hなどから参照)
-#define KEY_ESC   65307
-#define KEY_LEFT  65361
-#define KEY_RIGHT 65363
-#define KEY_W     119
-#define KEY_A     97
-#define KEY_S     115
-#define KEY_D     100
+# define KEY_ESC 65307
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+# define KEY_W 119
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
 
 // MinilibXのKeyPress / KeyRelease イベント
-#define EVENT_KEY_PRESS   2
-#define EVENT_KEY_RELEASE 3
+# define EVENT_KEY_PRESS 2
+# define EVENT_KEY_RELEASE 3
 
 // マスク (KeyPressMask, KeyReleaseMask)
-#define MASK_KEY_PRESS    (1L << 0)
-#define MASK_KEY_RELEASE  (1L << 1)
+# define MASK_KEY_PRESS (1L << 0)
+# define MASK_KEY_RELEASE (1L << 1)
 
-#define MAP_WIDTH  100
-#define MAP_HEIGHT 50
+# define MAP_WIDTH 100
+# define MAP_HEIGHT 50
 
-#define MOVE_SPEED 0.01  // 前後左右の移動速度
-#define ROT_SPEED -0.01  // 回転速度
+# define MOVE_SPEED 0.01 // 前後左右の移動速度
+# define ROT_SPEED -0.01 // 回転速度
+
+# define MINIMAP_OFFSET_X 10
+# define MINIMAP_OFFSET_Y 10
+# define ARROW_LEN 10.0
+# define MINIMAP_MAX_WIDTH 300
+# define MINIMAP_MAX_HEIGHT 300
+
+# define MOUSE_SENSITIVITY 0.001
 
 #endif
--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,124 +1,203 @@
 #ifndef CUB3D_H
 # define CUB3D_H
-#include "../minilibx-linux/mlx.h"
-#include <fcntl.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "../libft/libft.h"
-#include "config.h"
+# include "../libft/libft.h"
+# include "../minilibx-linux/mlx.h"
+# include "config.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 typedef struct s_player
 {
-    double posX;
-    double posY;
-    double dirX;
-    double dirY;
-    double planeX;
-    double planeY;
-}   t_player;
+	double		posX;
+	double		posY;
+	double		dirX;
+	double		dirY;
+	double		planeX;
+	double		planeY;
+}				t_player;
 
 typedef struct s_img
 {
-    void    *img_ptr;
-    int     *data;
-    int     width;
-    int     height;
-    int     bpp;
-    int     size_line;
-    int     endian;
-}   t_img;
+	void		*img_ptr;
+	int			*data;
+	int			width;
+	int			height;
+	int			bpp;
+	int			size_line;
+	int			endian;
+}				t_img;
 
 typedef struct s_keys
 {
-    int w;
-    int s;
-    int a;
-    int d;
-    int left;
-    int right;
-}   t_keys;
+	int			w;
+	int			s;
+	int			a;
+	int			d;
+	int			left;
+	int			right;
+}				t_keys;
 
 typedef struct s_ray
 {
-    double cameraX;
-    double rayDirX;
-    double rayDirY;
-    int mapX;
-    int mapY;
-    double sideDistX;
-    double sideDistY;
-    double deltaDistX;
-    double deltaDistY;
-    int stepX;
-    int stepY;
-    int hit;
-    int side;
-    double perpWallDist;
-    int lineHeight;
-    int drawStart;
-    int drawEnd;
-    double wallX;
-    int texX;
-    double step;
-    double texPos;
-    t_img *tex_ptr;
-}   t_ray;
+	double		cameraX;
+	double		rayDirX;
+	double		rayDirY;
+	int			mapX;
+	int			mapY;
+	double		sideDistX;
+	double		sideDistY;
+	double		deltaDistX;
+	double		deltaDistY;
+	int			stepX;
+	int			stepY;
+	int			hit;
+	int			side;
+	double		perpWallDist;
+	int			lineHeight;
+	int			drawStart;
+	int			drawEnd;
+	double		wallX;
+	int			texX;
+	double		step;
+	double		texPos;
+	t_img		tex_ptr;
+}				t_ray;
+
+typedef struct s_cub
+{
+	int			em_width;
+	int			em_height;
+	char		*texN_path;
+	char		*texS_path;
+	char		*texE_path;
+	char		*texW_path;
+	char		**e_map;
+	int			**visited;
+}				t_cub;
+
+typedef struct s_minimap
+{
+	int			left;
+	int			top;
+	int			right;
+	int			bottom;
+	int			color;
+	int			scale_x;
+	int			scale_y;
+	double		scale;
+	int			px;
+	int			py;
+	int			ex;
+	int			ey;
+	int			sx;
+	int			sy;
+	int			dx;
+	int			dy;
+	int			e2;
+	int			err;
+}				t_minimap;
 
 typedef struct s_vars
 {
-    void      *mlx;
-    void      *win;
-    t_player   player;
-    t_img      texN;
-    t_img      texS;
-    t_img      texE;
-    t_img      texW;
-    t_img      frame;   // フレームバッファ用
-    char      **map;
-    t_keys     keys;
-    int ceilingColor;
-    int floorColor;
-    t_ray ray;
-    char *texN_path;
-    char *texS_path;
-    char *texE_path;
-    char *texW_path;
-}   t_vars;
+	void		*mlx;
+	void		*win;
+	t_player	player;
+	t_img		texN;
+	t_img		texS;
+	t_img		texE;
+	t_img		texW;
+	t_img frame; // フレームバッファ用
+	char		**map;
+	t_keys		keys;
+	int			ceilingColor;
+	int			floorColor;
+	t_ray		ray;
+	char		*texN_path;
+	char		*texS_path;
+	char		*texE_path;
+	char		*texW_path;
+	t_cub		cub;
+	int			mouse_in_window;
+	int			old_x;
+	char		*cub_path;
+	int			map_height;
+	int			map_width;
+	t_minimap	minimap;
+}				t_vars;
 
-void move_player(t_vars *vars);
-void rotate_left(t_vars *vars);
-void rotate_right(t_vars *vars);
-int key_press(int keycode, t_vars *vars);
-int key_release(int keycode, t_vars *vars);
-int     get_tex_color(t_img *tex, int x, int y);
-void    my_mlx_pixel_put(t_img *img, int x, int y, int color);
-void print_map(char **map);
-void load_ber(char *file, t_vars *vars);
-void init_player_state(t_vars *vars);
-void clear_frame(t_vars *vars);
-void init_step_size_dist(t_vars *vars);
-void init_ray_vars(t_vars *vars, int x);
-void dda_loop(t_vars *vars);
-void calc_wall_distance_height(t_vars *vars);
-void calc_using_tex(t_vars *vars);
-void calc_tex_pos(t_vars *vars);
-void draw_wall(t_vars *vars, int x);
-void draw_walls(t_vars *vars);
-int render_frame(t_vars *vars);
-void init_key_state(t_keys *keys);
-void init_frame(t_vars *vars);
-int init_mlx(t_vars *vars);
-int close_window(t_vars *vars);
-
+void			debug_player_move(t_vars *vars);
+void			move_player(t_vars *vars);
+void			rotate_left(t_vars *vars);
+void			rotate_right(t_vars *vars);
+int				key_press(int keycode, t_vars *vars);
+int				key_release(int keycode, t_vars *vars);
+int				get_tex_color(t_img tex, int x, int y);
+void			my_mlx_pixel_put(t_img *img, int x, int y, int color);
+void			print_map(char **map);
+int				load_ber(char *file, t_vars *vars);
+void			init_player_state(t_vars *vars);
+void			clear_frame(t_vars *vars);
+void			init_step_size_dist(t_vars *vars);
+void			init_ray_vars(t_vars *vars, int x);
+void			dda_loop(t_vars *vars);
+void			calc_wall_distance_height(t_vars *vars);
+void			calc_using_tex(t_vars *vars);
+void			calc_tex_pos(t_vars *vars);
+void			draw_wall(t_vars *vars, int x);
+void			draw_walls(t_vars *vars);
+int				render_frame(t_vars *vars);
+void			init_key_state(t_keys *keys);
+int				init_frame(t_vars *vars);
+int				init_mlx(t_vars *vars);
+int				close_window(t_vars *vars);
+int				get_max_width(char **map);
+int				get_max_height(char **map);
+void			expand_map(t_vars *vars);
+int				validate_map(t_vars *vars);
+int				parse_cub(char *file, t_vars *vars);
+int				is_blank_line(char *line);
+void			free_vars(t_vars *vars);
+void			free_map(char **map);
+void			init_vars(t_vars *vars, char *argv1);
+int				is_map_char(char c);
+void			safe_exit_with_error(t_vars *vars, char *msg);
+void			parse_error(char *msg, t_vars *vars, int fd, char *line);
+void			free_split(char **split);
+int				string_to_color_value(char *line);
+char			*remove_spaces_tabs_newlines(char *line);
+int				rgb_to_int(char *line);
+int				split_len(char **split);
+char			*remove_spaces_tabs_newlines(char *line);
+int				tex_path_color_parsed(t_vars *vars);
+int				is_textures_xpm_extension(t_vars *vars);
+int				parse_tex_path_color(int fd, t_vars *vars);
+int				parse_map(int fd, t_vars *vars);
+void			allocate_visited(t_vars *vars);
+void			reset_visited(t_vars *vars);
+void			free_visited(t_vars *vars);
+int				validate_map(t_vars *vars);
+void			check_map_connected(t_vars *vars);
+int				validate_map_chars(char **map);
+int				is_map_char(char c);
+void			replace_enclosed_spaces_with_A(char **map, int height,
+					int width);
+int				validate_map_structures(char **map, int width, int height);
+int				get_max_width(char **map);
+int				get_max_height(char **map);
+int				is_reachable_zero_player(int start_row, int start_col,
+					int **visited, char **map, int height, int width);
+int				check_map_spaces_enclosed(char **map, int width, int height);
+void			check_map_enclosed(t_vars *vars);
+int				is_player_char(char c);
+int				mouse_move(int x, int y, t_vars *vars);
+int				mouse_enter(t_vars *vars);
+int				mouse_leave(t_vars *vars);
+void			draw_minimap(t_vars *vars);
+void			replace_interior_blanks(t_vars *vars);
+void			draw_player(t_vars *vars, double scale);
+int				trim_map_right_spaces(char **map, int height);
 #endif
